c308fe72511e69006ccf135adc79568d43cb45b0
Mario-junior95 <karehmario@gmail.com>
2020-10-21 13:12:18 +0300

    Completed Step3

--- index.js
+++ index.js
@@ -6,6 +6,21 @@ app.listen(port, () => {
   console.log(`listening at http://localhost:${port}`)
 })
 
-app.get(URL, (req, res, next) => {
+app.get("/", (req, res, next) => {
     res.send("Ok");
+});
+
+//when the url /test is invoked, answers: {status:200, message:"ok"}
+
+app.get("/test" , (reg , res , next) =>{
+    res.send('{status:200, message:"ok"}');
+});
+
+
+
+
+//when the url /time is invoked, answers with: {status:200, message:<TIME>}
+app.get("/time" , (reg , res , next) =>{
+    var currentTime =   new Date().toLocaleTimeString();
+    res.send('{status:200, message: ' + currentTime + '}');
 });
