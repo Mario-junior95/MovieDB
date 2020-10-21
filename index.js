ee77bc9c61155718142dabf0ddd915583fd232c4
Mario-junior95 <karehmario@gmail.com>
2020-10-21 14:22:20 +0300

    Completed Step4

--- index.js
+++ index.js
@@ -11,16 +11,42 @@ app.get("/", (req, res, next) => {
 });
 
 //when the url /test is invoked, answers: {status:200, message:"ok"}
-
-app.get("/test" , (reg , res , next) =>{
+app.get("/test" , (req , res , next) =>{
     res.send('{status:200, message:"ok"}');
 });
 
 
-
-
 //when the url /time is invoked, answers with: {status:200, message:<TIME>}
-app.get("/time" , (reg , res , next) =>{
+app.get("/time" , (req , res , next) =>{
     var currentTime =   new Date().toLocaleTimeString();
     res.send('{status:200, message: ' + currentTime + '}');
+});
+
+
+app.get("/test/:userId" , (req , res , next) =>{
+    res.send('{status:200, message:"Hello,' +  req.params.userId + '"}');
+});
+
+
+app.get('/search',(req,res,next) => {
+    const search = req.query.s;
+
+    if (typeof search != 'undefined') {
+        // Search string applied
+        const response = {
+    
+            status:200, message:"ok", data: search
+        };
+
+        res.send(response);
+    }
+    else {
+        const response = {
+            status:500, error:true, message: "you have to provide a search"
+        };
+
+
+        res.status(500);
+        res.send(response);
+    }
 });
